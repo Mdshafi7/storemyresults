f93e21846dc925999cd7b08ecb0d565c9ed202fa
QuickPrep <85073298+QuickPrep@users.noreply.github.com>
2021-09-20 17:44:44 +0530

    Delete php directory

--- php/results.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include_once 'php/config.php';
-session_start();
-
-$sql= mysqli_query($conn,"SELECT * FROM result where unique_id = '{$_SESSION['unique_id']}'");
-$row = mysqli_fetch_all($sql,MYSQLI_ASSOC);
-?>

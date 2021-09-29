c2db436c520e7767ef54b2dec88454ac8f75d2f3
QuickPrep <85073298+QuickPrep@users.noreply.github.com>
2021-09-29 12:46:46 +0530

    Delete results.sql

--- results.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `students` (
-  `student_id` int(11) NOT NULL AUTO_INCREMENT,
-  `unique_id` int(255) NOT NULL,
-  `fname` varchar(255) NOT NULL,
-  `lname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
